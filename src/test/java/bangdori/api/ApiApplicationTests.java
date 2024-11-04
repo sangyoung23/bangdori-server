de7d729c053c961ec47a3d2c964a49ea39ebb9fd
sangyong34 <bagsangyong34@gmail.com>
2024-11-04 15:57:14 +0900

    init commit

---
+++ src/test/java/bangdori/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bangdori.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
